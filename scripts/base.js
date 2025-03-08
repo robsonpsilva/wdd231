9f1f63d84e16016250bb388ae1474ac8ef8dd3d2
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-03-08 12:16:30 -0300

    modified:   scripts/base.js 	new file:   scripts/data.js

--- scripts/base.js
+++ scripts/base.js
@@ -1,6 +1,8 @@
+import { courses } from './data.js';
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.comp_nav');
 const listaItens = document.querySelectorAll('.comp_nav_a');
+const container = document.getElementById("courses-container");
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
@@ -25,3 +27,15 @@ function tWayFinder(item){
 		}
 	})
 }
+
+function filter(){
+	courses.forEach(course => {
+		const link = document.createElement("a");
+		link.href = `https://www.example.com/${course.subject}${course.number}`;
+		link.textContent = `${course.subject} ${course.number}`;
+		link.className = "link-button";
+		link.target = "_blank"; // Abre o link em nova aba
+		container.appendChild(link);
+	});
+}
+
