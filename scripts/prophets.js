9c01e22d384520a692d0d6f612a19e5823c84599
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-03-12 13:47:46 -0300

    modified:   .vscode/launch.json 	modified:   prophets.html 	modified:   scripts/prophets.js

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -11,8 +11,7 @@ async function getProphetData(url)
             throw new Error(`Error: ${response.status}`)
         }
         const data = await response.json();
-        console.table(data); 
-        return data;
+        displayProphets(data.prophets);
 
     }
     catch(error)
@@ -22,6 +21,30 @@ async function getProphetData(url)
     
 }
 
-getProphetData(url).then(data => {
 
-})
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+              // Create elements to add to the div.cards element
+              let card = document.createElement('section');
+              let fullName = document.createElement('h2'); // fill in the blank
+              let portrait = document.createElement('img');
+          
+              // Build the h2 content out to show the prophet's full name
+              fullName.innerHTML = `${prophet.name} <p>Date of birth: ${prophet.birthdate}</p>`; // fill in the blank
+              // Build the image portrait by setting all the relevant attributes
+              portrait.setAttribute('src', prophet.imageurl);
+              portrait.setAttribute('alt', `Portrait of ${prophet.name} ______________`); // fill in the blank
+              portrait.setAttribute('loading', 'lazy');
+              portrait.setAttribute('width', '340');
+              portrait.setAttribute('height', '440');
+          
+              // Append the section(card) with the created elements
+              card.appendChild(fullName); //fill in the blank
+              card.appendChild(portrait);
+          
+              cards.appendChild(card);
+            }); // end of arrow function and forEach loop
+}
+getProphetData(url);
+
+
