acb92727cd45e8cd351a64d54bfd97f391d7529d
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-03-12 21:09:29 -0300

    modified:   chamber/directory.html 	new file:   chamber/scripts/base.js 	new file:   chamber/styles/base.css 	new file:   chamber/styles/large.css 	modified:   index.html

---
+++ chamber/scripts/base.js
@@ -0,0 +1,27 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.comp_nav');
+const listaItens = document.querySelectorAll('.comp_nav_a');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    listaItens.forEach(item => {
+        item.addEventListener('click', () => {
+            tWayFinder(item)
+        });
+    });
+});
+
+function tWayFinder(item){
+	listaItens.forEach(elem => {
+		if (elem.textContent.trim() !== item.textContent.trim()) {
+			elem.classList.remove('active');
+		}
+		else{
+			elem.classList.add('active');
+		}
+	})
+}
