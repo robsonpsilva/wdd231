0575f5b61ae07c99a4912cbc514021b8cc94c7f3
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-03-26 09:36:36 -0300

    modified:   chamber/directory.html 	modified:   chamber/index.html 	new file:   chamber/join.html 	new file:   chamber/scripts/join.js 	new file:   chamber/styles/join.css

---
+++ chamber/scripts/join.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const timestampInput = document.getElementById('timestamp');
+    timestampInput.value = new Date().toISOString();
+});
+
+function openModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = "block";
+}
+
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = "none";
+    }
+}
